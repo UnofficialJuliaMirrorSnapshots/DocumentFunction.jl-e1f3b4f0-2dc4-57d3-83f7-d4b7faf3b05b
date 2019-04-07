52e85bc65d7ad2ad41ca1fb7c935b7bf4485e598
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-04-07 20:39:55 +0000

    Snapshot of branch master taken on 2019-04-07T16:39:53.973-04:00 from "https://github.com/UnofficialJuliaMirror/DocumentFunction.jl-e1f3b4f0-2dc4-57d3-83f7-d4b7faf3b05b"

---
+++ test/runtests.jl
@@ -0,0 +1,26 @@
+import DocumentFunction
+
+expected = "**DocumentFunction.documentfunction**\n\nCreate function documentation\n\nMethods:\n - `DocumentFunction.documentfunction(f::Function; location, maintext, argtext, keytext) in DocumentFunction`\nArguments:\n - `f::Function` : Function to be documented\nKeywords:\n - `argtext` : Dictionary with text for each argument\n - `keytext` : Dictionary with text for each keyword\n - `location` : Boolean to show/hide function location on the disk\n - `maintext` : General function description\n"
+
+import Test
+
+output = DocumentFunction.documentfunction(DocumentFunction.documentfunction;
+	location=false,
+	maintext="Create function documentation",
+	argtext=Dict("f"=>"Function to be documented"),
+	keytext=Dict("maintext"=>"General function description",
+				 "argtext"=>"Dictionary with text for each argument",
+				 "keytext"=>"Dictionary with text for each keyword",
+				 "location"=>"Boolean to show/hide function location on the disk"))
+
+DocumentFunction.documentfunction(DocumentFunction.stdoutcaptureon; location=true)
+DocumentFunction.getfunctionkeywords(DocumentFunction.stdoutcaptureon)
+DocumentFunction.getfunctionarguments(DocumentFunction.stdoutcaptureon)
+
+@Test.testset "Document" begin
+    @Test.test output == expected
+    @Test.test [] == DocumentFunction.getfunctionkeywords(DocumentFunction.stdoutcaptureon)
+    @Test.test [] == DocumentFunction.getfunctionarguments(DocumentFunction.stdoutcaptureon)
+end
+
+:passed
